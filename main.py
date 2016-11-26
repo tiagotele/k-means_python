36f56a73d2f583cc72ca029547be43dbdabc575a
Tiago Melo <tiagotele@gmail.com>
2016-11-26 16:34:51 -0300

    Finalizing x-means algorithm

--- main.py
+++ main.py
@@ -5,16 +5,8 @@ import csv
 import os
 import copy
 
-os.system('clear')
 
-database = []
-
-clusters_number = 3
-
-column_index = 0
-
-
-def carrega_csv(nome_do_arquivo, array_destino):
+def load_csv(nome_do_arquivo, array_destino):
     with open(nome_do_arquivo, 'r') as f:
         reader = csv.reader(f)
         for r in reader:
@@ -23,34 +15,21 @@ def carrega_csv(nome_do_arquivo, array_destino):
     f.close()
 
 
-carrega_csv('iris.csv', database)
+# First loop: to initiali split a specific row on equidistant centroids
+def generate_first_centroids_distribuition():
+    global centroid_index
+    global ratio_aux
 
-# Ratio of first split on my cluster
-ratio = len(database) // clusters_number
-print(ratio)
+    for database_line in range(len(database)):
 
-ratio_aux = ratio
+        key = ('c' + str(centroid_index))
+        if not (key in centroids):
+            centroids['c' + str(centroid_index)] = {}
+        centroids['c' + str(centroid_index)]['line' + str(database_line + 1)] = database[database_line][column_index]
 
-# Used to define the centroid index
-centroid_index = 0
-
-centroids = {}
-
-# First loop: to initiali split a specific row on equistant centroids
-for database_line in range(len(database)):
-
-    key = ('c' + str(centroid_index))
-    if not (key in centroids):
-        centroids['c' + str(centroid_index)] = {}
-    centroids['c' + str(centroid_index)]['line' + str(database_line + 1)] = database[database_line][column_index]
-
-    # print('c' + str(centroid_index) ,'-----', 'line' + str(database_line+1))
-
-    if database_line > ratio_aux:
-        print("ratio aux", ratio_aux)
-        print("centroid_index", centroid_index)
-        ratio_aux += ratio
-        centroid_index += 1
+        if database_line > ratio_aux:
+            ratio_aux += ratio
+            centroid_index += 1
 
 
 def get_closes_centroid_name(value):
@@ -79,18 +58,11 @@ print("----")
 
 def calculate_means_for_each_centroid():
     for centroids_index in range(len(centroids)):
-        print("Centroid index = ", centroids_index)
         mean = 0
         for item in centroids[str('c') + str(centroids_index)]:
-            # print(centroids[str('c') + str(centroids_index)][item])
             mean += float(centroids[str('c') + str(centroids_index)][item])
         mean /= float(len(centroids[str('c') + str(centroids_index)]))
         centroids[str('c') + str(centroids_index)]['mean'] = mean
-        print("Mean for centroid ", centroid_index, mean)
-
-
-centroids_changed = {}
-elements_changed = True
 
 
 # Iterate over each centroid
@@ -102,28 +74,50 @@ def alocate_elements_in_rigth_centroid():
         # Iterate over each centroid item
         for line in centroids[centroid]:
             if line != 'mean':
-                print(centroid, line, centroids[centroid][line], " closest to centroid ",
-                      get_closes_centroid_name(centroids[centroid][line]))
                 correct_centroid = get_closes_centroid_name(centroids[centroid][line])
                 if not correct_centroid == centroid:
-                    print("Should change")
                     centroids_changed[correct_centroid][line] = {}
                     centroids_changed[correct_centroid][line] = centroids_changed[centroid][line]
                     del centroids_changed[centroid][line]
                     elements_changed = True
-                else:
-                    print("Should not change")
 
 
-interations = 0
-while elements_changed :
-    interations += 1
-    calculate_means_for_each_centroid()
-    centroids_changed = copy.deepcopy(centroids)
-    alocate_elements_in_rigth_centroid()
-    centroids = copy.deepcopy(centroids_changed)
+os.system('clear')
 
-print("Interations = ", interations)
-print(centroids)
+database = []
 
+# Number of cluster to split the database
+clusters_number = 3
 
+# Column of database to be classified
+column_index = 0
+
+# Used when splitting database into clusters
+centroid_index = 0
+
+# Dictionary that holds database classified in clusters
+centroids = {}
+
+# Auxiliary dictionary
+centroids_changed = {}
+
+load_csv('iris.csv', database)
+
+# Ratio of first split on my cluster
+ratio = len(database) // clusters_number
+ratio_aux = ratio
+generate_first_centroids_distribuition()
+
+if __name__ == "__main__":
+
+    iterations = 0
+    elements_changed = True
+    while elements_changed:
+        iterations += 1
+        calculate_means_for_each_centroid()
+        centroids_changed = copy.deepcopy(centroids)
+        alocate_elements_in_rigth_centroid()
+        centroids = copy.deepcopy(centroids_changed)
+
+    print("Iterations = ", iterations)
+    print(centroids)
